fc753448a34498df2fbae62178759d8b51f5b5e1
Goodnews Adewole <goodnews.adewole@example.com>
2024-07-24 13:20:51 +0100

    done with mobile

--- src/Sections/CTA.jsx
+++ src/Sections/CTA.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Button from '../components/button'
+import TourImage from "../assets/book-a-tourImage.png"
+
+
 
 export default function CTA() {
   return (
-    <section id='cta' className=' bg-[#111828] text-white'>
-        <p className=''>
-            Are you looking for a co-working space?
-        </p>
-        <Button text={"Book A Tour"} style={"bg-primaryBlue"}/>
+    <section id='cta' className=' bg-[#111828] text-white pt-16 pb-8 tour-image'>
+        <div className='container mx-auto px-sm'>
+            <p className='syne text-[32px] font-medium'>
+                Are you looking for a co-working space?
+            </p>
+            <Button text={"Book A Tour"} style={"mt-5 bg-primaryBlue font-bold text-lg leading-[20px] font-[700] py-5 px-7 rounded-[40px] text-[18px]"}/>
+            <div className='mt-5 '>
+                <img src={TourImage}></img>
+            </div>
+        </div>
     </section>
   )
 }
--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import Button from '../components/button'
+import { PiPhoneBold } from 'react-icons/pi'
+import { CgMail } from 'react-icons/cg'
 
 export default function Footer() {
-    function ContactList ({icon, text}){
-        <p className='grid'> <span>{icon}</span> <span>{text}</span> </p>
+    function ContactList ({icon, children}){
+        return <p className='hover:cursor-pointer flex items-center text-lg leading-7 text-[#555555] gap-1'> <span className='text-primaryBlue text-2xl'>{icon}</span> <span>{children}</span> </p>
     }
+
   return (
-    <section id='contact'>
-        <p>Tavekhouse</p>
-        <div>
-            <p>Contact us</p>
-            <div>
-                <ContactList/>
+    <section id='contact' className='py-16 mx-auto container px-sm'>
+        <div className='mb-16'>
+            <p className='text-[28px] font-medium leading-[34px] syne '>Contact us</p>
+            <div className='grid gap-3 mt-5'>
+                <ContactList icon={<PiPhoneBold/>}>+2347016265809</ContactList>
+                <ContactList icon={<CgMail/>}>tavekhouse@gmail.com</ContactList>
             </div>
         </div>
-        <div className=' col-span-2 gap-3 pt-10  flex-col justify-center text-center xl:pt-0  xl:flex xl:flex-row'>
+         <p className='hover:cursor-pointer text-center opensans-italic font-extrabold tracking-[0.8px] text-3xl leading-[26px] underline  '>Tavek<span className=' text-primaryBlue '>House</span></p>
+        
+        <div className=' col-span-2 gap-3 pt-10 flex flex-col justify-center text-center xl:pt-0  xl:flex xl:flex-row text-lg text-[#555555]'>
             <a className='hover:text-primaryBlue' href='#home'>Home</a>
             <a className='hover:text-primaryBlue' href='#about'>About</a>
             <a className='hover:text-primaryBlue' href='#features'>Features</a>
             <a className='hover:text-primaryBlue' href='#location'>Locations</a>
             <a className='hover:text-primaryBlue' href='#cta'>CTA</a>
-            <Button text={"Contact Us"} link={"#contact"} style={"bg-primaryBlue"}>Contact Us</Button>
         </div>
     </section>
   )
